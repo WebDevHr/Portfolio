5a2727b810b33643e5c67fc85e0ef4ff0792a943
Hos Raz <hos.raz1993@gmail.com>
2023-04-18 02:33:29 +0300

    test

--- src/views/AboutMeView.vue
+++ src/views/AboutMeView.vue
@@ -2,9 +2,9 @@
   <div class="d-flex justify-content-center flex-column mt-5">
     <h1 class="text-">Bizim Hikaye</h1>
     <div>
-      <div class="fs-1 fontFamilyDancingScript mt-5 me-5">Huseyin</div>
+      <div class="fs-1 fontFamilyDancingScript mt-5 marginHuseyin">Huseyin</div>
       <div class="fs-2 love text-danger">Loves</div>
-      <div class="fs-1 fontFamilyDancingScript mb-5 ms-5">Esma</div>
+      <div class="fs-1 fontFamilyDancingScript mb-5 marginHuseyin">Esma</div>
     </div>
   </div>
 </template>
@@ -17,5 +17,11 @@ export default {};
 h1,
 .love {
   font-family: "Courgette", sans-serif;
+}
+.marginHuseyin {
+  margin-right: 100px;
+}
+.marginHuseyin {
+  margin-left: 100px;
 }
 </style>
