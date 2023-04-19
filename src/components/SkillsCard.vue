ed31a59d98ef01b4c1e7e69eec612d81fd07eda4
Hos Raz <hos.raz1993@gmail.com>
2023-04-19 03:13:30 +0300

    skills

--- src/components/SkillsCard.vue
+++ src/components/SkillsCard.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>
-    <img src="https://img.icons8.com/color/48/000000/vue-js.png" />
-    <div></div>
+  <div class="p-3 m-2 skillcard rounded">
+    <a :href="skillHref" class="text-decoration-none titleColor">
+      <img :src="icon" alt="Icon" width="100" height="100" />
+      <div class="titleColor fw-bold fontFamilyKalam">
+        <slot></slot></div
+    ></a>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    skillHref: {
+      type: String,
+      required: true,
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.skillcard {
+  transition: all 0.5s;
+}
+.skillcard:hover {
+  box-shadow: 5px 5px 15px rgb(147, 147, 147);
+  color: black;
+  scale: 1.05;
+  translate: 0px -10px;
+}
+.titleColor {
+  transition: all 0.5s;
+  color: grey;
+}
+.skillcard:hover .titleColor {
+  color: black;
+}
+</style>
--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -1,25 +1,112 @@
 <template>
-  <div class="container-lg">
-    <h1>Skills</h1>
-    <div>
+  <div class="container-lg fontFamilyKalam">
+    <h1 class="fw-bold fontSize fontFamilyCourgette mb-5">Skills</h1>
+    <div class="fs-4">
       This a List of Technologies that i worked with and have a pretty good
       understading of them.
     </div>
-    <div class="d-flex flex-wrap m-3">
-      <div>
-        <img src="https://img.icons8.com/color/100/000000/vue-js.png" />
-        <div>VueJS</div>
-      </div>
-      <div>
-        <img src="https://img.icons8.com/color/100/000000/nodejs.png" />
-        <div>NodeJs</div>
-      </div>
+
+    <div class="d-flex flex-wrap justify-content-center m-3">
+      <SkillsCard
+        v-for="(skill, index) in skills"
+        :key="skill.id"
+        :icon="skill.src"
+        :skillHref="skill.href"
+        :draggable="true"
+        @dragstart="dragStart(index)"
+        @dragover="dragOver($event)"
+        @dragleave="dragLeave($event)"
+        @drop="drop(index)"
+      >
+        {{ skill.title }}</SkillsCard
+      >
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import SkillsCard from "@/components/SkillsCard.vue";
+export default {
+  components: {
+    SkillsCard,
+  },
+  data() {
+    return {
+      skills: [
+        {
+          id: 1,
+          title: "JavaScript",
+          src: "https://img.icons8.com/color/200/000000/javascript.png",
+          href: "https://www.javascript.com/",
+        },
+        {
+          id: 2,
+          title: "VueJS",
+          src: "https://img.icons8.com/color/200/000000/vue-js.png",
+          href: "https://vuejs.org/",
+        },
+        {
+          id: 3,
+          title: "NodeJS",
+          src: "https://img.icons8.com/color/200/000000/nodejs.png",
+          href: "https://nodejs.dev/en/",
+        },
+        {
+          id: 4,
+          title: "MongoDB",
+          src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+          href: "https://www.mongodb.com/",
+        },
+        {
+          id: 5,
+          title: "Bootstarp",
+          src: "https://img.icons8.com/color/200/000000/bootstrap.png",
+          href: "https://getbootstrap.com",
+        },
+        {
+          id: 6,
+          title: "ExpressJS",
+          src: "https://img.icons8.com/color/200/000000/express-js.png",
+          href: "https://expressjs.com",
+        },
+        {
+          id: 7,
+          title: "HTML",
+          src: "https://img.icons8.com/color/200/000000/html-5.png",
+          href: "https://www.w3.org/html/",
+        },
+        {
+          id: 8,
+          title: "CSS",
+          src: "https://img.icons8.com/color/200/000000/css3.png",
+          href: "https://www.w3schools.com/css/",
+        },
+        {
+          id: 9,
+          title: "C++",
+          src: "https://img.icons8.com/color/200/000000/c-plus-plus-logo.png",
+          href: "https://www.w3schools.com/css/",
+        },
+        {
+          id: 10,
+          title: "MySQL",
+          src: "https://img.icons8.com/fluent/200/000000/mysql-logo.png",
+          href: "https://www.mysql.com/",
+        },
+        {
+          id: 11,
+          title: "Git",
+          src: "https://img.icons8.com/color/200/000000/git.png",
+          href: "https://git-scm.com/",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.fontSize {
+  font-size: 50px;
+}
+</style>
