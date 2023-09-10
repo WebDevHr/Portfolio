e61d3978a072a5fa4ed2bfca049912957a2d8ec7
Hossein Razeghian <razeqian@gmail.com>
2023-09-10 18:56:44 +0300

    all

--- src/components/navigationMenu/NavbarComp.vue
+++ src/components/navigationMenu/NavbarComp.vue
@@ -1,55 +1,26 @@
 <template>
-  <div
-    class="d-flex flex-row align-items-center flex-wrap flex-lg-nowrap justify-content-between"
-  >
-    <div
-      class="col-6 col-lg-3 d-flex justify-content-center align-items-center order-1"
-    >
+  <div class="d-flex flex-row align-items-center flex-wrap flex-lg-nowrap justify-content-between">
+    <div class="col-6 col-lg-3 d-flex justify-content-center align-items-center order-1">
       <transition appear>
         <div
           class="d-flex justify-content-center align-items-center rounded-circle size-of-logo shadow logo cursorPointer"
-          @mouseover="logoOver"
-          @mouseleave="logoOut"
-          @click="goToHome"
-        >
-          <img
-            src="@/assets/pictures/hrLogo.png"
-            alt="HR-logo"
-            width="50"
-            height="50"
-          />
+          @mouseover="logoOver" @mouseleave="logoOut" @click="goToHome">
+          <img src="@/assets/pictures/hrLogo.png" alt="HR-logo" width="50" height="50" />
         </div>
       </transition>
     </div>
 
-    <div
-      class="col-10 col-lg-6 order-3 order-lg-2 mt-lg-0 mt-3 mx-auto mx-lg-0 menu"
-      v-show="isClicked"
-    >
-      <div
-        v-for="(item, index) in navTabs"
-        :key="index"
+    <div class="col-10 col-lg-6 order-3 order-lg-2 mt-lg-0 mt-3 mx-auto mx-lg-0 menu" v-show="isClicked">
+      <div v-for="(item, index) in navTabs" :key="index"
         class="py-2 px-3 px-md-2 px-xl-3 mx-2 my-0 fs-3 rounded item navItems cursorPointer fw-bold"
-        @click="goToRelated(item)"
-      >
+        @click="goToRelated(item)">
         {{ item }}
       </div>
     </div>
-    <div
-      class="col-6 col-lg-3 d-flex flex-row order-2 order-lg-3 justify-content-center"
-    >
-      <div
-        class="col-3 d-flex justify-content-center align-items-center menuButton"
-      >
-        <div
-          role="button"
-          class="navbar-burger burger"
-          :class="{ 'is-active': isActive }"
-          @click="toggleNavbar"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarMenu"
-        >
+    <div class="col-6 col-lg-3 d-flex flex-row order-2 order-lg-3 justify-content-center">
+      <div class="col-3 d-flex justify-content-center align-items-center menuButton">
+        <div role="button" class="navbar-burger burger" :class="{ 'is-active': isActive }" @click="toggleNavbar"
+          aria-label="menu" aria-expanded="false" data-target="navbarMenu">
           <span></span>
           <span></span>
           <span></span>
@@ -136,25 +107,31 @@ export default {
 
 <style scoped>
 @import url("@/assets/fonts.css");
+
 * {
   font-family: "DancingScript", sans-serif;
 }
+
 .fontFamilyDancingScript {
   font-family: "DancingScript", sans-serif;
 }
+
 .size-of-logo {
   width: 60px;
   height: 60px;
 }
+
 .hamburger {
   width: 50px;
   height: 50px;
 }
+
 .hamburger span {
   width: 30px;
   height: 5px;
   background: #000;
 }
+
 .navbar-burger {
   border: none;
   background-color: transparent;
@@ -209,15 +186,18 @@ export default {
   top: 0.85rem;
   left: 0;
 }
+
 .navItems {
   color: rgb(150, 150, 150);
   float: right;
   transition: color 0.5s, box-shadow 0.5s;
 }
+
 .navItems:hover {
   color: black;
   box-shadow: 5px 5px 15px rgb(176, 176, 176);
 }
+
 .floatLeft {
   float: left;
 }
@@ -225,6 +205,7 @@ export default {
 .cursorPointer {
   cursor: pointer;
 }
+
 @media screen and (max-width: 991px) {
   .menu {
     display: flex;
@@ -232,6 +213,7 @@ export default {
     background-color: white;
   }
 }
+
 .menu {
   display: flex;
   justify-content: center;
